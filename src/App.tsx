1eb9ebc340d05014354886171413536420ea5ee6
phongvanngo <19520834@gm.uit.edu.vn>
2021-11-04 08:34:10 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "styles/global.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import store from "redux/store";
+import "./i18n";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <div className="App">
+          <Home />
+        </div>
+      </Provider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
