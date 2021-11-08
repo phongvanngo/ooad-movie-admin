e146cfde976624e6bafc1e53fba9c0eaa3f64703
phongvanngo <19520834@gm.uit.edu.vn>
2021-11-08 11:06:04 +0700

    update

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import AdminRoutes from "./routes";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 export default function AdminPage(): ReactElement {
 	return (
